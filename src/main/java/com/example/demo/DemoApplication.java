6c512ee313a5695a4822c279be6150d1a9073753
kirijav25 <kirijav25@gmail.com>
2026-08-05 13:28:37 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,12 @@
-
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Pipeline");
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
